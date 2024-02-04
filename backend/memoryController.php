2a4967e024c95bbf9b3aad0951eb67ba18d23434
CrisafulliPasquale <101709329+CrisafulliPasquale@users.noreply.github.com>
2024-02-04 00:38:22 +0000

    ultimi aggiustamenti

--- backend/memoryController.php
+++ backend/memoryController.php
@@ -1,36 +1,18 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['board'])) {
-    $_SESSION['board'] = array_fill(0, 20, 'hidden');
+if (!isset($_SESSION['attempts'])) {
     $_SESSION['attempts'] = 3;
 }
 
-if (isset($_POST['action'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $action = $_POST['action'];
-    if ($action == 'reset') {
-        $_SESSION['board'] = array_fill(0, 20, 'hidden');
+
+    if ($action === 'reset') {
         $_SESSION['attempts'] = 3;
-        echo json_encode($_SESSION['board']);
-    } elseif ($action == 'flip') {
-        $index = $_POST['index'];
-        $_SESSION['board'][$index] = 'visible';
-        echo json_encode($_SESSION['board']);
-    } elseif ($action == 'check') {
-        $index1 = $_POST['index1'];
-        $index2 = $_POST['index2'];
-        if ($_SESSION['board'][$index1] == 'visible' && $_SESSION['board'][$index2] == 'visible') {
-            $_SESSION['board'][$index1] = 'matched';
-            $_SESSION['board'][$index2] = 'matched';
-        } else {
-            $_SESSION['attempts']--;
-            if ($_SESSION['attempts'] <= 0) {
-                $_SESSION['board'] = array_fill(0, 20, 'hidden');
-                $_SESSION['attempts'] = 3;
-            }
-        }
-        echo json_encode($_SESSION['board']);
+    } elseif ($action === 'decrease') {
+        $_SESSION['attempts']--;
     }
-} else {
-    echo json_encode($_SESSION['board']);
+
+    echo $_SESSION['attempts'];
 }
