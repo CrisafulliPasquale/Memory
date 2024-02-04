655018c587f9e45f0b9cedf6dc3f2b9f3e0f1306
CrisafulliPasquale <101709329+CrisafulliPasquale@users.noreply.github.com>
2024-02-04 00:40:38 +0000

    agg

--- backend/memoryController.php
+++ backend/memoryController.php
@@ -2,14 +2,14 @@
 session_start();
 
 if (!isset($_SESSION['attempts'])) {
-    $_SESSION['attempts'] = 3;
+    $_SESSION['attempts'] = 5;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $action = $_POST['action'];
 
     if ($action === 'reset') {
-        $_SESSION['attempts'] = 3;
+        $_SESSION['attempts'] = 5;
     } elseif ($action === 'decrease') {
         $_SESSION['attempts']--;
     }
